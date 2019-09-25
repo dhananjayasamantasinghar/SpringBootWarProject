a4df4361569f2aaa3107c43ab685341dcce2b30e
dhananjayasamantasinghar <52064780+dhananjayasamantasinghar@users.noreply.github.com>
2019-09-25 08:19:07 +0530

    Upload

---
+++ src/main/java/com/demo/SpringBootWarProject/RestResource.java
@@ -0,0 +1,15 @@
+package com.demo.SpringBootWarProject;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/rest")
+public class RestResource {
+
+	@GetMapping("/msg")
+	public String getMessage(){
+		return "Welcome Dhananjaya!!!";
+	}
+}
